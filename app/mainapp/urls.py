b86fde587a26003e5e8d5487adc479cc9138cadc
polina <pnechaeva@betuple.com>
2023-02-14 20:05:24 +0300

    first commit

---
+++ app/mainapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from mainapp.views import MainListView, EmailView
+
+app_name = 'mainapp'
+
+urlpatterns = [
+    path('', MainListView.as_view(), name='index'),
+    path('email/', EmailView.as_view(), name='send_email'),
+]
